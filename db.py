6f7ad82d659dae81df21d5254d7f490003417fca
Yu-Shiang <z58085111@gmail.com>
2018-12-25 21:11:12 +0800

    fix wrong var

--- db.py
+++ db.py
@@ -73,7 +73,7 @@ class RegisterMail():
 	def send(self):
 		header = {'content-type': 'application/json'}
 		jsonBody = self.genReqBody()
-		requests.post(self.AzureMailService, headers = header, data = jsonBody)
+		requests.post(self.mailServiceUrl, headers = header, data = jsonBody)
 
 class RegisterDB(DB):
 
